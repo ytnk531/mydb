7d3d2c9e944ce77eda430805d113d363e3828456
ytnk531 <ytnk531@gmail.com>
2021-12-22 22:01:18 +0900

    persist data

--- exe/mydb.rb
+++ exe/mydb.rb
@@ -1,14 +1,17 @@
 require 'mydb'
 
+Signal.trap(:SIGINT) { Mydb::Store.get.persist }
+
 loop do
   input = gets
 
   begin
     command = Mydb::Parser.new.parse(input)
     command.run
-  # rescue => e
-  #   puts e.inspect
-  #   puts e.cause
+    Mydb::Store.get.persist
+#  rescue => e
+#    puts e.inspect
+#    puts e.cause
   end
 end
 
--- lib/mydb/store.rb
+++ lib/mydb/store.rb
@@ -1,11 +1,25 @@
 module Mydb
   class Store
     def self.get
-      @@instance ||= Store.new
+      @@instance ||= restore
+    end
+
+    def self.restore
+      begin
+        tables = if File.exist?("data")
+          Marshal.load(File.open("data", "rb"))
+                 else
+                   {}
+                 end
+      rescue EOFError
+        STDERR.puts "err"
+        tables = {}
+      end
+      Store.new(tables)
     end
   
-    def initialize
-      @tables = {}
+    def initialize(tables = {})
+      @tables = tables
     end
   
     def add(table)
@@ -18,6 +32,12 @@ module Mydb
   
     def table(table_name)
       @tables[table_name]
+    end
+
+    def persist
+      File.open("data", "wb") do
+        Marshal.dump(@tables, _1)
+      end
     end
   end
 end
